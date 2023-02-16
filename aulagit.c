533e9aae0ee8605713f7a616d6f33ef4573ba7d7
Lucas Paulo da Rocha <1711208@adms.mackenzie.br>
2023-02-16 16:41:49 -0300

    Teste VS Code Mackenzie

--- aulagit.c
+++ aulagit.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
     
 int main(){
-    char saida;
+    char opt;
 
-    printf("Hello, World!");
-    scanf("%c", saida);
+    printf("Isso é só um teste ;)");
+
+    do{
+        printf("\nMenu: \n (1) - Continuar\n (2) - Sair\n > ");
+        scanf("%c", opt);
+        switch (opt)
+        {
+        case '1':
+            continue;
+        
+        default:
+            return 0;
+            break;
+        }
+    } (while opt != '2');
+    
     return 0;
 }
