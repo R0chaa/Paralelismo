4c56423dcbebb73a9c2a6daeb6a41b4b4489257d
Lucas Paulo da Rocha <1711208@adms.mackenzie.br>
2023-02-16 16:52:40 -0300

    Terminal do lab - Teste

--- aulagit.c
+++ aulagit.c
@@ -1,23 +1,75 @@
 #include <stdio.h>
-    
+
+int EhImparPar(char *entrada);
+
 int main(){
-    char opt;
+    int opt;
+    char *entrada = "aba";
 
     printf("Isso é só um teste ;)");
 
     do{
-        printf("\nMenu: \n (1) - Continuar\n (2) - Sair\n > ");
-        scanf("%c", opt);
+        printf("\nMenu: \n (1) - Teste goto\n (2) - Sair\n > ");
+        scanf("%d", &opt);
         switch (opt)
         {
-        case '1':
+        case 1:
+            printf("\nSaída: %d", EhImparPar(entrada));
             continue;
         
         default:
-            return 0;
             break;
         }
-    } (while opt != '2');
+    } while (opt != 2);
     
     return 0;
+}
+
+int EhImparPar(char *entrada) {
+  
+  AParBPar:
+    if (*entrada == 'a') {
+      entrada++;
+      goto AImparBPar;
+    } else if (*entrada == 'b') {
+      entrada++;
+      goto AParBImpar;
+    } else {
+       return 0;
+    }
+
+  AImparBPar:
+    if (*entrada == 'a') {
+      entrada++;
+      goto AParBPar;
+    } else if (*entrada == 'b') {
+      entrada++;
+      goto AImparBImpar;
+    }
+    else {
+      return 1;
+    }
+
+  AImparBImpar:
+    if (*entrada == 'a') {
+      entrada++;
+      goto AParBImpar;
+    } else if (*entrada == 'b') {
+      entrada++;
+      goto AImparBPar;
+    }
+     else {
+       return 0;
+    }
+  
+  AParBImpar:
+    if (*entrada == 'a') {
+      entrada++;
+      goto AImparBImpar;
+    } else if (*entrada == 'b') {
+      entrada++;
+      goto AParBPar;
+      } else {
+       return 0;
+    }
 }
