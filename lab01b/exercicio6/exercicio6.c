dfa1f5bb5ddcab60f030957849b9224c00544e43
Ubuntu <lucas.rcha04@gmail.com>
2023-03-13 13:13:39 +0000

    exercicio 6

--- lab01b/exercicio6/exercicio6.c
+++ lab01b/exercicio6/exercicio6.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LIN 3
-#define COL 2
+int LIN = 3, COL = 2;
 
 int imprime(int** matriz){
 	printf("\n");
 	for(int i=0; i<LIN; i++){
 		for(int j=0; j<COL; j++){
-			printf("%d", matriz[i][j]);
+			printf(" %d", matriz[i][j]);
 		}
 		printf("\n");
 	}
@@ -30,6 +29,7 @@ int **transpor(int **ptr){
 
 int main(){
 	int **matriz = (int **)malloc(LIN*sizeof(int*));
+	int aux;
 	for(int i=0; i<LIN; i++) matriz[i] = (int *) malloc(COL * sizeof(int));
 
 	for(int i=0; i<LIN; i++){
@@ -40,5 +40,10 @@ int main(){
 	imprime(matriz);
 
 	int **transposta = transpor(matriz);
+
+	aux = LIN;
+	LIN = COL;
+	COL = aux;
+
 	imprime(transposta);
 }
