1fe297a3114f6a26dff6ed41805b9fba825315e8
lucas <leatt@protonmail.com>
2023-03-28 21:20:33 -0300

    lab03

--- lab03/memoria.c
+++ lab03/memoria.c
@@ -8,19 +8,19 @@
 int main()
 {
     pid_t childpid;
-    
     int shmid;
     
     shmid = shmget (IPC_PRIVATE, sizeof (int), 0666|IPC_CREAT);
     
     int *ipcPointer = (int *) shmat (shmid, 0, 0);
     *ipcPointer = 1;
-       
-     if ((childpid = fork()) == 0)
+    
+    childpid = fork();
+    
+     if (childpid == 0)
     {
         printf("Valor inicial: %i\n", *ipcPointer);
         printf ("ID Filho após fork: %d\n", getpid());
-        
         *ipcPointer += 2;
         printf("Valor após soma: %i\n", *ipcPointer);
     } 
@@ -29,7 +29,7 @@ int main()
         wait(NULL);
         *ipcPointer *= 4;
         printf("Valor multiplicado por 4 após soma do filho: %i\n", *ipcPointer);
+        shmdt(ipcPointer); //detach operation
     }
-
     return 0;
 }
