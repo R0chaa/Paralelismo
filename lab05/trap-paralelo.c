3a4eb8a1874fa69776f348ab255a013c6e0f0d90
lucas rocha <73042947+R0chaa@users.noreply.github.com>
2023-04-25 18:49:40 -0300

    Update trap-paralelo.c

--- lab05/trap-paralelo.c
+++ lab05/trap-paralelo.c
@@ -3,8 +3,6 @@
 #include <omp.h>
 #include <math.h>
 
-#define n 1000000000
-
 void Trap (double a, double b, int n, double* global_result_p);
 
 double f(double x);
@@ -13,6 +11,7 @@ int main(int argc, char* argv[])
 {
 	double global_result = 0.0;
 	double a = 0.0, b = 100.0;
+	int n = 1000000000;
 	int thread_count;
 
 	thread_count = strtol(argv[1], NULL, 10);
