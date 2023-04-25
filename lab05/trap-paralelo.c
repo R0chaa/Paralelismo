28b07d9d1f016b959824c1c2aaafb22afbdaf7f3
lucas rocha <73042947+R0chaa@users.noreply.github.com>
2023-04-25 18:26:52 -0300

    remove input

--- lab05/trap-paralelo.c
+++ lab05/trap-paralelo.c
@@ -3,14 +3,15 @@
 #include <omp.h>
 #include <math.h>
 
-void Trap(double a, double b, int n, double* global_result_p);
+void Trap (double a, double b, int n, double* global_result_p);
 
 double f(double x);
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[])
+{
 	double global_result = 0.0;
-	double a, b;
-	int n;
+	double a = 0.0, b = 100.0;
+        int n = 1000000000;
 	int thread_count;
 
 	thread_count = strtol(argv[1], NULL, 10);
@@ -39,10 +40,12 @@ void Trap(double a, double b, int n, double* global_result_p)
 	local_a = a + my_rank*local_n*h;
 	local_b = local_a + local_n*h;
 	my_result = (f(local_a) + f(local_b))/2.0;
+	
 	for(i = 1; i <= local_n-1; i++){
 		x = local_a + i*h;
 		my_result += f(x);
 	}
+	
 	my_result = my_result*h;
 #	pragma omp critical
 	*global_result_p += my_result;
