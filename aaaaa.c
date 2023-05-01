ffcd34d01db8e6d26abbc02fe0b4b8df0a9838e4
Ubuntu <lucas.rcha04@gmail.com>
2023-05-01 19:12:30 +0000

    pelo amor de deus

---
+++ aaaaa.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <gmp.h>
+
+/*
+void fatorial(mpf_t result, int n) {
+    mpf_set_ui(result, 1);
+    for (int i = 2; i <= n; i++) {
+        mpf_mul_ui(result, result, i);
+    }
+}
+*/
+
+void euler(mpf_t result, int n) {
+    //mpf_set_ui(result, 1);
+    mpf_t fact, term;
+    mpf_init(fact);
+    mpf_init(term);
+
+    mpf_set_ui(fact, 1);
+    mpf_set_ui(term, 0);
+
+    for (int i = 1; i < n; i++) {
+        //factorial(fact, i);
+        mpf_mul_ui(fact, fact, i);
+	mpf_ui_div(term, 1, fact);
+        mpf_add(result, result, term);
+    }
+
+    mpf_clear(fact);
+    mpf_clear(term);
+}
+
+int main() {
+    mpf_set_default_prec(80000U);
+    mpf_t e;
+    mpf_init(e);
+    euler(e, 10000);
+    gmp_printf("Euler: %.9999Ff\n", e);
+    mpf_clear(e);
+    return 0;
+}
